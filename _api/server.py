70d1c7f5be8a75d922d5f017c7577b712ed3a495
Timothy Green <timothy.green@gmail.com>
2014-02-19 15:44:08 +0000

    add better error handling to postcode api server

--- _api/server.py
+++ _api/server.py
@@ -21,6 +21,11 @@ def postcode():
 
   url = "http://mapit.mysociety.org/postcode/{}".format(postcode[:8])
   resp = requests.get(url)
+
+  # Invalid postcode
+  if resp.status_code != 200:
+    return abort(500, "Invalid mapit response, {}".format(resp.status_code))
+
   data = resp.json()
 
   for _, area in data['areas'].items():
@@ -28,6 +33,9 @@ def postcode():
       return jsonify({'name': area['name'],
                       'id': slugify(area['name']),
                       'url': region_url.format(slugify(area['name']))})
+
+  # E.g. for postcode like ASCN 1ZZ not in EU
+  return abort(500, "Could not find EU region")
 
 if __name__ == "__main__":
     app.run(debug = True, host='0.0.0.0', port=7777)
