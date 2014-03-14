2d3ea476a161c99a357dcbaf7d41813e68a4fd31
Tim Green <timothy.green@gmail.com>
2014-03-14 00:38:05 +0000

    rename newsletter server to reminder

---
+++ _reminder/local_settings.py
@@ -0,0 +1,14 @@
+MAIL_SERVER = "email-smtp.eu-west-1.amazonaws.com"
+MAIL_PORT=465
+MAIL_USE_SSL=True
+MAIL_USERNAME = "AKIAIKSHOIJJ3QCRNCRA"
+MAIL_PASSWORD = "As+nTh6K/RXq8xMHA7xHwYIe3DCiGbg5KtHhLwyIve/9"
+
+MAIL_SENDER = "noreply@yournextmep.com"
+
+#MAIL_SERVER='smtp.gmail.com'
+#MAIL_PORT=465
+#MAIL_USE_SSL=True
+#MAIL_USERNAME = 'timothy.green@gmail.com'
+#MAIL_PASSWORD = 'ctzfsycirymfqzib'
+
