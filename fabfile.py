29629e06767e4670840716952f2343cfaf3ec708
Timothy Green <timothy.green@gmail.com>
2014-03-20 14:19:57 +0000

    start of twitter images in fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,21 @@
+import os
 from fabric.api import env, run, cd, local
 
-env.hosts = ['yournextmep.com']
+env.hosts = ['yournextmep@yournextmep.com']
 
 def deploy():
   push()
   pull()
   build()
 
+def twitter_images():
+  with cd('~/site/'):
+    for f in os.listdir('_data'): 
+      if f.endswith('_people.yaml'):
+        print f
+
 def push():
-  local('git push')
+  local('git push origin master')
 
 def pull():
   with cd('~/site/'):
